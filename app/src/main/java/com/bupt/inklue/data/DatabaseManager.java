cc3ecd7df23029e186e80fa0d9aa9b452258c572
wiki <1225959684@qq.com>
2024-06-25 20:12:09 +0800

    增加汉字和练习数量

--- app/src/main/java/com/bupt/inklue/data/DatabaseManager.java
+++ app/src/main/java/com/bupt/inklue/data/DatabaseManager.java
@@ -57,13 +57,21 @@ public class DatabaseManager {
                 "不", "之", "山", "廿", "古",
                 "四", "日", "石", "六", "天",
                 "上", "下", "工", "山", "千",
-                "人", "大", "九", "之", "心"));
+                "人", "大", "九", "之", "心",
+                "十", "七", "干", "王", "主",
+                "人", "之", "子", "白", "史",
+                "入", "之", "大", "天", "文",
+                "丁", "中", "公", "万", "石"));
         ArrayList<String> classNames = new ArrayList<>(Arrays.asList(
                 "U571F0000", "U738B0000", "U4E940000", "U4E0A0000", "U4E0B0000",
                 "U4E0D0000", "U4E4B0000", "U5C710000", "U5EFF0000", "U53E40000",
                 "U56DB0000", "U65E50000", "U77F30000", "U516D0000", "U59290000",
                 "U4E0A0100", "U4E0B0100", "U5DE50100", "U5C710100", "U53430100",
-                "U4EBA0100", "U59270100", "U4E5D0100", "U4E4B0100", "U5FC30100"));
+                "U4EBA0100", "U59270100", "U4E5D0100", "U4E4B0100", "U5FC30100",
+                "U53410200", "U4E030201", "U5E720200", "U738B0200", "U4E3B0200",
+                "U4EBA0200", "U4E4B0200", "U5B500200", "U767D0200", "U53F20200",
+                "U51650300", "U4E4B0300", "U59270300", "U59290300", "U65870300",
+                "U4E010300", "U4E2D0300", "U516C0300", "U4E070300", "U77F30300"));
         for (int i = 0; i < chars.size(); i++) {
             String c = chars.get(i);
             String className = classNames.get(i);
@@ -78,16 +86,26 @@ public class DatabaseManager {
             db.insert("StdChar", null, values);
         }
         ArrayList<String> names = new ArrayList<>(Arrays.asList(
-                "峄山碑·壹", "峄山碑·贰", "峄山碑·叁", "九成宫醴泉铭·壹", "九成宫醴泉铭·贰"));
+                "峄山碑·壹", "峄山碑·贰", "峄山碑·叁",
+                "九成宫醴泉铭·壹", "九成宫醴泉铭·贰",
+                "曹全碑·壹", "曹全碑·贰",
+                "张猛龙碑·壹", "张猛龙碑·贰"));
         ArrayList<String> charIDs = new ArrayList<>(Arrays.asList(
-                "1,2,3,4,5", "6,7,8,9,10", "11,12,13,14,15", "16,17,18,19,20", "21,22,23,24,25"));
+                "1,2,3,4,5", "6,7,8,9,10", "11,12,13,14,15",
+                "16,17,18,19,20", "21,22,23,24,25",
+                "26,27,28,29,30", "31,32,33,34,35",
+                "36,37,38,39,40", "41,42,43,44,45"));
         ArrayList<ArrayList<CharData>> charsDataList = new ArrayList<>();
         charsDataList.add(new ArrayList<>(charsData.subList(0, 5)));
         charsDataList.add(new ArrayList<>(charsData.subList(5, 10)));
         charsDataList.add(new ArrayList<>(charsData.subList(10, 15)));
         charsDataList.add(new ArrayList<>(charsData.subList(15, 20)));
         charsDataList.add(new ArrayList<>(charsData.subList(20, 25)));
-        for (int i = 0; i < charsDataList.size(); i++) {
+        charsDataList.add(new ArrayList<>(charsData.subList(25, 30)));
+        charsDataList.add(new ArrayList<>(charsData.subList(30, 35)));
+        charsDataList.add(new ArrayList<>(charsData.subList(35, 40)));
+        charsDataList.add(new ArrayList<>(charsData.subList(40, 45)));
+        for (int i = 0; i < names.size(); i++) {
             ContentValues values = new ContentValues();
             String name = names.get(i);
             String coverImgPath = dirPath + "/cover/" + name + ".jpg";
@@ -99,5 +117,31 @@ public class DatabaseManager {
             values.put("charIDs", charIDs.get(i));
             db.insert("Practice", null, values);
         }
+        putTestData(db, dirPath);//置入测试数据
+    }
+
+    //置入测试数据
+    private static void putTestData(SQLiteDatabase db, String dirPath) {
+        String c = "土";
+        String className = "U571F0000";
+        String stdImgPath = dirPath + "/char/" + className + ".jpg";
+        ContentValues charValues = new ContentValues();
+        charValues.put("name", c);
+        charValues.put("className", className);
+        charValues.put("stdImgPath", stdImgPath);
+        long charID = db.insert("StdChar", null, charValues);
+        ContentValues practiceValues = new ContentValues();
+        String practiceName = "测试";
+        String coverImgPath = dirPath + "/cover/" + practiceName + ".jpg";
+        PracticeData practiceData = new PracticeData();
+        CharData charData = new CharData();
+        practiceData.charsData = new ArrayList<>();
+        charData.setStdImgPath(stdImgPath);
+        practiceData.charsData.add(charData);
+        BitmapProcessor.createCover(practiceData, coverImgPath, true);
+        practiceValues.put("name", practiceName);
+        practiceValues.put("coverImgPath", coverImgPath);
+        practiceValues.put("charIDs", String.valueOf(charID));
+        db.insert("Practice", null, practiceValues);
     }
 }
